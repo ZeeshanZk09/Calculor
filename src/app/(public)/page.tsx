b9ed80912e993745000b39796c11f3bbeb09ca73
Muhammad Zeeshan Khan <dr5269139@gmail.com>
2024-10-04 12:10:16 +0500

    aksbfadsk

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+// import About from './about/page'
+export default function Home() {
+  return (
+    <div className="flex justify-center min-h-screen p-1 pb-20 gap-6 sm:p-20 font-[family-name:var(--font-geist-sans)]" >
+          <h1 className="text-4xl">Hello World</h1>
+          {/* <About /> */}
+          {/* <br /> */}
+          <h2 className="text-4xl">From GIAIC</h2>
+    </div>
+  );
+}
